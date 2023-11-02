005593d4ddcf7065210ba56885666b758758b7a2
facuft2 <facusebaperez@gmail.com>
2023-11-02 20:04:48 -0300

    Functional register without validate inputs

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { registerUser } = require('../controllers/auth.controller');
 
-router.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+router.post('/register', async ( {body}, res) => {
+  try { 
+    const { result, code } = await registerUser(body)
+
+    console.log(result, code, 'result, code')
+    switch (code) {
+      case 201:
+        return res.status(code).json({ user: result })
+      case 400:
+        return res.status(code).json({ error: result })
+    }
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+}
+)
 
 module.exports = router;
